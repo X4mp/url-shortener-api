d5f6642cb5c22ddf985452ff620a96c7fc7ecb3c
Your Name <you@example.com>
2017-03-02 19:47:53 +0000

    initial

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+var fs = require('fs');
+var validator = require('valid-url');
+var dirty = require('dirty');
+var db = dirty('url.db');
+
+var count = 1001;
+
+app.get('/', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var fileStream = fs.createReadStream('index.html');
+    fileStream.pipe(res);
+});
+
+app.get('/new/:url*', function(req, res) {
+    console.log('new');
+    var url = req.url.slice(5);
+    if(!validator.isUri(url)) {
+        res.write(JSON.stringify({'error' : 'Invalid Url format'}));
+        res.end();
+        return
+    } 
+
+    if(typeof(db.get(url)) == 'undefined') {
+        db.set(url, count.toString());
+        console.log('write ' + url + '  db');
+        hash = count;
+        count += 1;        
+    } else {
+        hash = db.get(url);
+    } 
+    var shorturl = 'https://herko.com/' + hash.toString();
+    res.write(JSON.stringify({'short-url' : shorturl}));
+    res.end() 
+
+});
+
+app.get('/:url', function(req, res) {
+    console.log('redirect');
+    var url = req.url.slice(1);
+    if(typeof(db.get(url)) == 'undefined') {
+        res.write(JSON.stringify({'error' : 'unknown short-url'}));
+        res.end();
+    }
+
+    res.redirect('https://losfuzzys.github.io');
+    res.end();
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('Example app listening on port 3000')
+});
